503389a8e84222aa1532290779785574b277e3df
EmmanuelR12 <developer7@grupotransforma.mx>
2024-10-25 17:31:37 -0600

    comimt

---
+++ HolaMundo/src/EjemploStringValidar.java
@@ -0,0 +1,13 @@
+public class EjemploStringValidar {
+    public static void main(String[] args) {
+        String curso = null;
+
+        boolean esNulo = curso == null;
+        System.out.println("esNulo = " + esNulo);
+
+        if(esNulo == false ){
+            System.out.println("curso = " + curso.toUpperCase());
+        }
+
+    }
+}
